8b1b48e2e17902b2f92d626df2f887eb959c606f
WIN10\yaliun <funnytago@gmail.com>
2020-06-30 00:34:00 +0900

    Unity Rigidbody2D를 이용한 Move

---
+++ Example/Assets/Script/MoveOfRigidbody.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MoveOfRigidbody : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;                 //이동 속도
+    private Rigidbody2D rigid2D;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");   //좌우 이동
+        float y = Input.GetAxisRaw("Vertical");     //상하 이동
+
+        // Rigidbody2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+    }
+}
